a1e4c20925934ba61b9331dc4a7eed97106d3585
dpak1999 <deepakkumardash313@gmail.com>
2021-08-18 21:40:25 +0530

    add : questions

---
+++ utils.js
@@ -0,0 +1,60 @@
+/** @format */
+
+export const questionsArray = [
+  {
+    question:
+      "What is the secret identity of the Arrow in the television series Arrow?",
+    options: ["Tommy merlyn", "Oliver queen", "John diggle"],
+    answer: "Oliver queen",
+  },
+  {
+    question:
+      "What is the name of the yacht that sinks and leaves Oliver Queen stranded in the premiere episode of Arrow?",
+    options: ["The queen", "the arrow", "The queen's gambit"],
+    answer: "The queen's gambit",
+  },
+  {
+    question:
+      "In the series Arrow, who is character Oliver Queen's best friend?",
+    options: ["Tommy merlyn", "Roy harper", "Quentin lance"],
+    answer: "Tommy merlyn",
+  },
+  {
+    question:
+      "What is the name of the island where Oliver Queen is marooned in the series Arrow?",
+    options: ["Lian Yu", "Yian Lu", "Yeo fei"],
+    answer: "Lian Yu",
+  },
+  {
+    question: " What is the hometown of Arrow charcter Olive queen?",
+    options: ["Coast city", "Keystone city", "Starling city"],
+    answer: "Starling city",
+  },
+  {
+    question: "Which Arrow character says 'You have failed this city!'",
+    options: ["The dark archer", "The huntress", "The arrow"],
+    answer: "The arrow",
+  },
+  {
+    question:
+      "Who is Oliver Queen's girlfriend before the shipwreck in the Arrow series?",
+    options: ["Felicity smoak", "Laurel lance", "Amanda waller"],
+    answer: "Laurel lance",
+  },
+  {
+    question: "What is Arrow character John Diggle's profession?",
+    options: ["Bodyguard", "Detective", "Defence"],
+    answer: "Bodyguard",
+  },
+];
+
+export const highScore = [
+  {
+    name: "Ras",
+    score: 8,
+  },
+  {
+    name: "Ghul",
+    score: 7,
+  },
+];
